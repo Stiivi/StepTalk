d5bb9925c8a22f0d0e30a23f78ce43efa0ab1d20
Stefan Urbanek <stefan@agentfarms.net>
2020-08-29 21:42:36 +0800

    Initial Commit

---
+++ StepTalk/StepTalk.h
@@ -0,0 +1,19 @@
+//
+//  StepTalk.h
+//  StepTalk
+//
+//  Created by Stefan Urbanek on 2020/8/29.
+//  Copyright © 2020 Stefan Urbanek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StepTalk.
+FOUNDATION_EXPORT double StepTalkVersionNumber;
+
+//! Project version string for StepTalk.
+FOUNDATION_EXPORT const unsigned char StepTalkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StepTalk/PublicHeader.h>
+
+
---
+++ StepTalkTests/StepTalkTests.m
@@ -0,0 +1,37 @@
+//
+//  StepTalkTests.m
+//  StepTalkTests
+//
+//  Created by Stefan Urbanek on 2020/8/29.
+//  Copyright © 2020 Stefan Urbanek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StepTalkTests : XCTestCase
+
+@end
+
+@implementation StepTalkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
